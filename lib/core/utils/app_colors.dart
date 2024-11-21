592f993e17b24a34c8c5ac02bada8f4bd5deb241
Rasha Alaswad <rasha.n.alaswad@gmail.com>
2024-11-21 18:16:25 +0200

    structure the app folders

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color primaryBlue = Color(0xFF247CFF);
+  static const Color lightBlue = Color(0xFFF4F8FF);
+  static const Color darkBlue = Color(0xFF242424);
+}
