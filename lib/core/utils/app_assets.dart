f65a2bbfa5ce6ffd6fc06af602767d1dc8c3565f
Rasha Alaswad <rasha.n.alaswad@gmail.com>
2024-11-22 06:38:11 +0200

    - add assets folder - add app_assets.dart - add onboarding assets

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,10 @@
+class AppAssets {
+  static const String imageBasePath = 'assets/images/';
+  static const String svgBasePath = 'assets/svgs/';
+
+  static const String onboardingDoctor =
+      '${imageBasePath}onboarding_doctor.png';
+  static const String appLogo = '${svgBasePath}app_logo.svg';
+  static const String onboardingBackground =
+      '${svgBasePath}onboarding_background.svg';
+}
