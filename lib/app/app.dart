29e76a123d3e0e1d45da65d028d650cbc3ec6589
Rasha Alaswad <rasha.n.alaswad@gmail.com>
2024-11-22 10:50:04 +0200

    add app theme file

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:journey_to_advanced_flutter/core/theme/app_theme.dart';
 
 import '../core/router/app_router.dart';
 import '../core/router/routes.dart';
@@ -17,6 +18,7 @@ class MainApp extends StatelessWidget {
       splitScreenMode: true,
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
+        theme: appTheme(),
         initialRoute: Routes.onboarding,
         onGenerateRoute: appRouter.onGenerateRoute,
       ),
---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:journey_to_advanced_flutter/core/utils/app_colors.dart';
+
+ThemeData appTheme() {
+  return ThemeData(
+    scaffoldBackgroundColor: Colors.white,
+    primaryColor: AppColors.primaryBlue,
+  );
+}
